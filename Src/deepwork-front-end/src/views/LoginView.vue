5863cbe8fc7fbe6baaff190796e261d796b8d161
Hamza KHANFARA <khanfarahamza@gmail.com>
2024-02-26 23:46:45 +0100

    Creating Login View

---
+++ Src/deepwork-front-end/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<template>
+  <Navbar />
+  <div class="container">
+    <h2>Login</h2>
+    <div class="login-page-wrapper">
+      <LoginPage />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Navbar from '@/components/NavigationBar.vue'
+import LoginPage from '@/components/LoginPage.vue'
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Center content horizontally */
+}
+
+.login-page-wrapper {
+  margin-top: 20px; /* Add some space between Navbar and LoginPage */
+}
+</style>
