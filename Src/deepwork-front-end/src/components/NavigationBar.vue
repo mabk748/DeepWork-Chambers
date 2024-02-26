d4b671982e537d0653a7fc5658d244e8f684dd97
Hamza KHANFARA <khanfarahamza@gmail.com>
2024-02-26 23:47:12 +0100

    aditing components and views

--- Src/deepwork-front-end/src/components/NavigationBar.vue
+++ Src/deepwork-front-end/src/components/NavigationBar.vue
@@ -1,22 +1,29 @@
 <template>
-  <a-layout-header>
-    <div class="logo">MonSite</div>
-    <a-input
-      placeholder="Rechercher..."
-      style="width: 200px;"
-    />
-    <a-menu
-      theme="light"
-      mode="horizontal"
-      :default-selected-keys="['1']"
-      style="line-height: 64px;"
-    >
-      <a-menu-item key="1">Accueil</a-menu-item>
-      <a-menu-item key="2">À Propos</a-menu-item>
-      <a-menu-item key="3">Services</a-menu-item>
-      <a-menu-item key="4">Contact</a-menu-item>
-    </a-menu>
-  </a-layout-header>
+  <div class="Navbar-style">
+    <a-layout-header>
+      <div class="Navbar-components-style">
+        <div class="logo">DeepWork</div>
+        <a-input
+          placeholder="Rechercher..."
+          style="width: 200px;"
+        />
+        <div style="position: absolute; right: 0px;">
+          <a-menu
+          theme="light"
+          mode="horizontal"
+          :default-selected-keys="['1']"
+          style="line-height: 64px;"
+        >
+          <a-menu-item key="1">Accueil</a-menu-item>
+          <a-menu-item key="2">À Propos</a-menu-item>
+          <a-menu-item key="3">Login</a-menu-item>
+        </a-menu>
+        </div>
+        
+      </div>
+      
+    </a-layout-header>
+  </div>
 </template>
 
 <script lang="ts">
@@ -30,25 +37,22 @@ export default defineComponent({
     AMenuItem: Menu.Item,
     AInput: Input
   },
-  data() {
-    return {
-      theme: 'dark', // Choisissez entre 'dark' ou 'light' selon votre préférence
-      mode: 'horizontal' // Choisissez entre 'horizontal' ou 'vertical' selon votre préférence
-    };
-  }
 });
 </script>
 
 <style scoped>
+.Navbar-components-style {
+  display: flex;
+  align-items: center;
+  
+}
+.Navbar-style{
+  border-bottom: #919191;
+}
 .logo {
-  color: #fff;
-  float: left;
+  color: #000;
   font-size: 1.5em;
   margin-right: 20px;
   line-height: 64px;
 }
-
-/* Styles spécifiques à votre barre de navigation peuvent être ajoutés ici */
 </style>
-
-  
--- Src/deepwork-front-end/src/views/HomeView.vue
+++ Src/deepwork-front-end/src/views/HomeView.vue
@@ -1,16 +1,56 @@
 <template>
   <div class="home">
     <Navbar />
+    <div class="Home-content-style">
+      <h1>{{ pageTitle }}</h1>
+      <a-button type="primary" @click="goToLogin">Get started</a-button>
+    </div>
+    <div class="footer">
+      <p>Footer of the homepage to think about</p>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { Button } from 'ant-design-vue';
 import Navbar from '@/components/NavigationBar.vue'
 export default defineComponent({
   name: 'HomeView',
   components: {
-    Navbar
+    Navbar,
+    AButton: Button
   },
+  setup(){
+    const router = useRouter();
+    const goToLogin = () => {
+      router.push('/login');
+    };
+    return {
+      goToLogin
+    };
+  },
+  data() {
+    return {
+      pageTitle: 'DeepWork Chambers'
+    };
+  }
 });
 </script>
+<style scoped>
+.Home-content-style{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  margin: 100px;
+};
+.footer {
+  background-color: #f0f2f5;
+  padding: 20px;
+  text-align: center;
+  margin-top: 100px;
+  bottom: 0;
+}
+</style>
