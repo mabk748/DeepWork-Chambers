ceff183d58d96c3c39ef979888c34d8b76693cce
Hamza KHANFARA <khanfarahamza@gmail.com>
2024-02-27 11:38:04 +0100

    adding pinia to my app

--- Src/deepwork-front-end/src/main.ts
+++ Src/deepwork-front-end/src/main.ts
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createPinia } from 'pinia'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+// Créer une instance Pinia
+const pinia = createPinia()
+
+// Installer Pinia dans l'application Vue
+app.use(pinia)
+
+// Utiliser le routeur et monter l'application
+app.use(router).mount('#app')
