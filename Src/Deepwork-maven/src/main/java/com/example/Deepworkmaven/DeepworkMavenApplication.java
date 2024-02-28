380c95f00a85348931cbf983cecdbed39751e6a1
Hamza KHANFARA <khanfarahamza@gmail.com>
2024-02-28 16:06:24 +0100

    setting up backend project based on java Spring

---
+++ Src/Deepwork-maven/src/main/java/com/example/Deepworkmaven/DeepworkMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.Deepworkmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepworkMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepworkMavenApplication.class, args);
+	}
+
+}
